77a987dac100309bebc9dae154eb5fe3a61fb8c5
Muhammad Waqas <m.waqas3341@gmail.com>
2026-06-30 05:44:06 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   // ── Supabase ────────────────────────────────────────────────────
-  SUPABASE_URL: 'https://xxxx.supabase.co',      // <-- your project URL
+  SUPABASE_URL: 'https://bnvrblekeppkpvcjjpli.supabase.co/rest/v1/',      // <-- your project URL
   SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJudnJibGVrZXBwa3B2Y2pqcGxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MTEyMjIsImV4cCI6MjA5ODI4NzIyMn0.9qmk8Msddi5FwQPY6SvM4WXVQiZUjLXJEsjxHdsgw68',        // <-- anon/public key
 
   // ── App identity ─────────────────────────────────────────────────
